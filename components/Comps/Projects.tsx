1370ab948970f0f46b79628e1281b9581cb32b13
j-ami-l <mdhasanjamil0001@gmail.com>
2025-10-17 09:55:12 +0600

    //

--- components/Comps/Projects.tsx
+++ components/Comps/Projects.tsx
@@ -3,7 +3,7 @@ import React, { useRef } from 'react';
 import { motion, useScroll, useTransform } from 'framer-motion';
 import Image from 'next/image';
 import Link from 'next/link';
-import { FaGithub, FaExternalLinkAlt } from 'react-icons/fa';
+import {  FaExternalLinkAlt } from 'react-icons/fa';
 
 const projects = [
     {
